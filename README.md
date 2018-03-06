e6f89eee4b2497c8760030c0ad223472af2fe121
ryantc94 <rtc270@nyu.edu>
2018-03-05 21:37:49 -0500

    socket server added

--- README.md
+++ README.md
@@ -1,42 +1,66 @@
 # kill_the_werewolf
-Deception Game
+##Deception Game
+<p> This will be based off Mafia. Basically there is a werewolf and other characters. The characters need to figure out who the werewolf is. Some characters will have special abilities. </p>
+<p> TODO: Elaborate... </p>
 
-Home Page
+## Architecture
+I guess this can be a isomorphic app, client side for everything except the actually game itself, but I guess redux ajax can handle the client side as well... I don't know but let's get started.
+### Express Backend
+* ^Socket server will be in backend with WS library
+### Create-React-App Frontend
+* ^React Router for routing
+
+
+##Wireframes
+###Home Page
+<br />
 <img src='wireframes/Home_Page.png' width='500' height='500'/>
 
-Choose Amount of Players and Discussion Time
+### Choose Amount of Players and Discussion Time
+<br />
 <img src='wireframes/Player_Page.png' width='500' height='500'/>
 
-Website Wireframe for Recieving Roles
+### Website Wireframe for Recieving Roles
+<br />
 <img src='wireframes/Website_Role_Choosing.png ' width='500' height='500'/>
 
-Mobile Wireframe for Recieving Roles
+### Mobile Wireframe for Recieving Roles
+<br />
 <img src='wireframes/Mobile_Role_Choosing.png' width='500' height='500'/>
 
-Day Time
+### Day Time
+<br />
 <img src='wireframes/Day.png' width='500' height='500'/>
 
-Who Died?
+### Who Died?
+<br />
 <img src='wireframes/Player_Death.png' width='500' height='500'/>
 
-Group Discussion
+### Group Discussion
+<br />
 <img src='wireframes/Discussion.png' width='500' height='500'/>
 
-Night Time
+### Night Time
+<br />
 <img src='wireframes/Night.png' width='500' height='500'/>
 
-Website Werewolves Choose Who to Eat
+### Website Werewolves Choose Who to Eat
+<br />
 <img src='wireframes/Werewolves_Discuss.png' width='500' height='500'/>
 
-Mobile Werewolves Choose Who to Eat
+### Mobile Werewolves Choose Who to Eat
+<br />
 <img src='wireframes/Mobile_Werewolves_Choose.png' width='500' height='500'/>
 
-Results
+### Results
+<br />
 <img src='wireframes/Results.png' width='500' height='500'/>
 
-Rules Page
+### Rules Page
+<br />
 <img src='wireframes/Rules.png' width='500' height='500'/>
 
-About Page
+### About Page
+<br />
 <img src='wireframes/About.png' width='500' height='500'/>
 
--- werewolf_server/app.js
+++ werewolf_server/app.js
@@ -1,14 +1,34 @@
+/**
+ * Module dependencies.
+ */
+
 var express = require('express');
 var path = require('path');
 var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var http = require('http');
+var debug = require('debug')('werewolf-server:server');
+
 
 var index = require('./routes/index');
 var users = require('./routes/users');
 
+/**
+ * Create HTTP server.
+ */
+
 var app = express();
+var httpServer = http.createServer(app);
+var socketServer = require('socket.io')(httpServer);
+
+/**
+ * Get port from environment and store in Express.
+ */
+
+var port = normalizePort(process.env.PORT || '8000');
+app.set('port', port);
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -23,6 +43,9 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+/*
+	Routes
+*/
 app.use('/', index);
 app.use('/users', users);
 
@@ -43,5 +66,74 @@ app.use(function(err, req, res, next) {
   res.status(err.status || 500);
   res.render('error');
 });
+
+/**
+ * Normalize a port into a number, string, or false.
+ */
+
+function normalizePort(val) {
+  var port = parseInt(val, 10);
+
+  if (isNaN(port)) {
+    // named pipe
+    return val;
+  }
+
+  if (port >= 0) {
+    // port number
+    return port;
+  }
+
+  return false;
+}
+
+/**
+ * Event listener for HTTP server "error" event.
+ */
+
+function onError(error) {
+  if (error.syscall !== 'listen') {
+    throw error;
+  }
+
+  var bind = typeof port === 'string'
+    ? 'Pipe ' + port
+    : 'Port ' + port;
+
+  // handle specific listen errors with friendly messages
+  switch (error.code) {
+    case 'EACCES':
+      console.error(bind + ' requires elevated privileges');
+      process.exit(1);
+      break;
+    case 'EADDRINUSE':
+      console.error(bind + ' is already in use');
+      process.exit(1);
+      break;
+    default:
+      throw error;
+  }
+}
+
+/**
+ * Event listener for HTTP server "listening" event.
+ */
+
+function onListening() {
+  var addr = httpServer.address();
+  var bind = typeof addr === 'string'
+    ? 'pipe ' + addr
+    : 'port ' + addr.port;
+  debug('Listening on ' + bind);
+}
+
+
+/**
+ * Listen on provided port, on all network interfaces.
+ */
+
+httpServer.listen(port);
+httpServer.on('error', onError);
+httpServer.on('listening', onListening);
 
 module.exports = app;
