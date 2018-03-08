196c4690a52c06da00a8f1404f94183d7d91a768
ryantc94 <rtc270@nyu.edu>
2018-03-08 00:31:35 -0500

    TotalPlayers component is setup need to read up on how to write submit and forms for React to finish it up

--- werewolf_client/src/Components/TotalPlayers.js
+++ werewolf_client/src/Components/TotalPlayers.js
@@ -2,13 +2,58 @@ import React from 'react'
 
 import '../SASS/_totalPlayers.css'
 
+/*
+Forms (React and HTML)
+
+Form tag wraps the entire form, everything from input to submit.
+
+<input name | type | placeHolder | value | onChange | required>
+
+*/
+
 class TotalPlayers extends React.Component {
+	constructor() {
+		super()
+		this.state = {
+			playerNumber: null
+		}
+	}
+
+	// why does using arrow function fix the setState error?
+	onInputPlayers = e => {
+		this.setState({ [e.target.name] : e.target.value})
+	}
+
+	onInputSubmit = e => {
+		console.log(typeof(this.state.playerNumber))
+	}
+
 	render() {
 		return (
 			<div className='totalPlayers'>
-				<div className='choose-players-text'> 
-					Please Choose From 5 - 9 Players
+				<div className='choose-players-text-container'> 
+					<h2 className='choose-players-text'> 
+						Please choose between 5 - 9 players 
+					</h2>
 				</div>
+				<form className='choose-players-form'>
+					<div className='chooser-player-input-container'>
+						<input
+							name='playerNumber'
+							type='text'
+							placeholder='Choose 5 - 9 Players'
+							value={this.state.playerNumber}
+							onChange={this.onInputPlayers}
+							required
+						/> 
+					</div>
+					<div className='chooser-player-input-error'>
+						<h3> I told you to choose between 5 - 9... inclusively...</h3>
+					</div>
+					<div className='chooser-player-input-submit'>
+						<input type='submit' value='Start' onSubmit={this.onInputSubmit}/>
+					</div> 
+				</form>
 			</div>
 		)
 	}
