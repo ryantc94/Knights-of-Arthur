3a13a9ac3fe15d5f8b87fa3984f33d7bebb9344e
Ryan <ryan.chen@jcrew.com>
2018-10-23 01:15:29 -0400

    Revert to last working version

--- werewolf_server/db.js
+++ werewolf_server/db.js
@@ -1,4 +1,5 @@
 var mongoose = require('mongoose')
+const { mongoDB } = require('./configuration.js')
 
 var Character = new mongoose.Schema({
 	_id: Number,
@@ -26,22 +27,22 @@ mongoose.model('Character', Character);
 
 //huh..... look into this code
 // is the environment variable, NODE_ENV, set to PRODUCTION? 
-if (process.env.NODE_ENV == 'PRODUCTION') {
-	// if we're in PRODUCTION mode, then read the configration from a file
-	// use blocking file io to do this...
-	var fs = require('fs');
-	var path = require('path');
-	var fn = path.join(__dirname, 'config.json');
-	var data = fs.readFileSync(fn);
+// if (process.env.NODE_ENV == 'PRODUCTION') {
+// 	// if we're in PRODUCTION mode, then read the configration from a file
+// 	// use blocking file io to do this...
+// 	var fs = require('fs');
+// 	var path = require('path');
+// 	var fn = path.join(__dirname, 'config.json');
+// 	var data = fs.readFileSync(fn);
 
-	// our configuration file will be in json, so parse it and set the
-	// conenction string appropriately!
-	var conf = JSON.parse(data);
-	var dbconf = conf.dbconf;
-} else {
-// if we're not in PRODUCTION mode, then use
-	dbconf = mongoose.connect('mongodb://localhost/koaDB')
-}
+// 	// our configuration file will be in json, so parse it and set the
+// 	// conenction string appropriately!
+// 	var conf = JSON.parse(data);
+// 	var dbconf = conf.dbconf;
+// } else {
+// // if we're not in PRODUCTION mode, then use
+// 	dbconf = mongoose.connect('mongodb://localhost/koaDB')
+// }
 
-mongoose.connect(dbconf);
+mongoose.connect(`mongodb://${mongoDB.username}:${mongoDB.password}@ds125872.mlab.com:25872/avalon`);
 
